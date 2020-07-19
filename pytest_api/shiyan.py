d406acf3aee4ecbb770d3c95b06beca8e3048b93
littltprince <1124479307@qq.com>
2020-07-19 21:41:11 +0800

    logging

---
+++ pytest_api/shiyan.py
@@ -0,0 +1,6 @@
+import logging
+logging.debug('this is the debug ')
+logging.info('this is the info ')
+logging.warning('this is the warning ')
+logging.error('this is the error ')
+logging.critical('this is the critical ')
